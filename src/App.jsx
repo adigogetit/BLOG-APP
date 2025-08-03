73f6db3e917334daed69c96d9c3e63996664d16d
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2025-08-03 18:37:55 +0530

    final file structure ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import React from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import RootLayout from './layout/rootLayout'
+import Home from './pages/home'
+import About from './pages/about'
+
+const App = () => {
+    const router = createBrowserRouter(createRoutesFromElements(
+        <Route path='/' element={<RootLayout/>}>
+            <Route index element ={<Home/>}/>
+            <Route path='about' element ={<About/>}/>
+        </Route>    
+    ))
+
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default App
 
